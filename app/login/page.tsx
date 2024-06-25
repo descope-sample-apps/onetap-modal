ad7f829ec987344075a8d1928946001a3b5e5e1d
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2024-06-25 11:33:43 -0700

    Add chatMode query param pass through

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,21 @@
+'use client'
+
+import { useEffect, useState } from 'react';
 import { flowId } from '@/lib/constants';
 import { Descope } from '@descope/nextjs-sdk';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 export default function Login() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const chatMode = searchParams.get('chatMode');
+
     return (
         <Descope
             flowId={flowId}
-            redirectAfterSuccess="/"
+            redirectAfterError=''
+            redirectUrl=''
+            onSuccess={() => router.push(`/${chatMode ? `?chatMode=${chatMode}` : ''}`)}
         />
     );
 }
--- components/features/AuthenticationDialog.tsx
+++ components/features/AuthenticationDialog.tsx
@@ -1,23 +1,34 @@
 'use client'
 
+import { useState } from 'react';
 import {
     Dialog,
     DialogContent,
     DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 import { Descope } from "@descope/nextjs-sdk";
 import { Button } from "@/components/ui/button";
 import { flowId } from "@/lib/constants";
+import { useSearchParams } from 'next/navigation';
 
 export default function AuthenticationDialog() {
-    return <Dialog>
-        <DialogTrigger>
-            <Button>Sign up or in</Button>
-        </DialogTrigger>
-        <DialogContent className="min-h-[700px]">
-            <Descope
-                flowId={flowId}
-            />
-        </DialogContent>
-    </Dialog>;
+    const [isOpen, setIsOpen] = useState(false);
+
+    const searchParams = useSearchParams();
+    const chatMode = searchParams.get('chatMode');
+
+    const getRedirectUrl = () => {
+        return `${window.location.origin}/login?${chatMode ? `chatMode=${chatMode}` : ''}`;
+    }
+
+    return (
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <DialogTrigger asChild>
+                <Button>Sign up or in</Button>
+            </DialogTrigger>
+            <DialogContent className="min-h-[700px]">
+                <Descope flowId={flowId} redirectUrl={getRedirectUrl()} />
+            </DialogContent>
+        </Dialog>
+    );
 }
