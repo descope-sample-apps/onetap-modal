d8621e466d6eacc3d2571f3dd06d393be8106dac
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2024-08-21 09:15:54 -0700

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,13 +2,19 @@
 
 import { flowId } from '@/lib/constants';
 import { Descope } from '@descope/nextjs-sdk';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 export default function Login() {
     const router = useRouter();
+    return <Suspense fallback={null}><LoginContent router={router} /></Suspense>;
+  
+}
+
+function LoginContent({ router } : { router: AppRouterInstance }) {
     const searchParams = useSearchParams();
     const chatMode = searchParams.get('chatMode');
-
     return (
         <Descope
             flowId={flowId}
